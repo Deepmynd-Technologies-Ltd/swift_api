7180658d319a388cbbfd04de750cdf5197c82464
AAdevdtudios <oladelejoseph01@gmail.com>
2024-09-20 04:12:21 +0100

    For cron

--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,10 @@ from authentication.services import get_user_account, register_login
 
 auth_router = Router(tags=["Authentication"])
 
+@auth_router.get("/cron", auth=None)
+def cron_job(request):
+  return auth_router.api.create_response(request, {"good": "Work"}, status=200)
+
 @auth_router.post("create-user/", response=ResponseDTO, auth=None)
 def create_user(request, token:GoogleTokenDTO):
   res = register_login(token)
