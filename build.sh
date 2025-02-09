57568fb3db6cbca733ba9ad9f6767104bc2dc150
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 17:40:01 +0100

    Comment all migrations

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -o errexit  # Exit immediately if a command exits with a non-zero status
 
-python manage.py makemigrations
-python manage.py showmigrations
-python manage.py migrate
-python manage.py collectstatic --no-input || echo "Static collection failed, continuing..."
+# python manage.py makemigrations
+# python manage.py showmigrations
+# python manage.py migrate
+# python manage.py collectstatic --no-input || echo "Static collection failed, continuing..."
 
 # Corrected if statement syntax
 # if [ "$CREATE_SUPERUSER" = "true" ]; then
