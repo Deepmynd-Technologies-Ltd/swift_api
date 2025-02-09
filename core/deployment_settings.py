d3b432ebb8ca879f1b2e9629fe1b05a8e01fe7e2
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 10:46:34 +0100

    requirement.txt updated

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -4,7 +4,6 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-print(ALLOWED_HOSTS)
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
