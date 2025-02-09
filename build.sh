d76ec66fdd3459dadcf66453cdc3558556264f0e
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 18:15:00 +0100

    Added the code to build.sh for createsuperuser

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 gunicorn core.wsgi --bind 0.0.0.0:8000
+
+if [[$CREATE_SUPERUSER == "true"]];
+then
+    echo "Creating superuser"
+    python manage.py createsuperuser --no-input
+fi
