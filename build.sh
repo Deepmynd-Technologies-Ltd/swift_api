40426a346524d33d03815578e5b30f81a7c86660
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 12:41:07 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-set -o errexit
+set -o errexit  # Exit immediately if a command exits with a non-zero status
+
 python manage.py makemigrations
 python manage.py showmigrations
 python manage.py migrate
 python manage.py collectstatic --no-input || echo "Static collection failed, continuing..."
 
-
-if [[$CREATE_SUPERUSER == "true"]];
-then
-    echo "Creating superuser"
-    python manage.py createsuperuser --no-input
-fi
+# Corrected if statement syntax
+# if [ "$CREATE_SUPERUSER" = "true" ]; then
+#     echo "Creating superuser"
+#     python manage.py createsuperuser --no-input
+# fi
