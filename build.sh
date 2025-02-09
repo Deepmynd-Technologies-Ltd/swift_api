c91d42c951514bcbaa52c3e9bd0fa3e10fe83421
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 17:58:46 +0100

    return build.sh to it previous phase

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-set -o errexit  # Exit immediately if a command exits with a non-zero status
-
-# python manage.py makemigrations
-# python manage.py showmigrations
-# python manage.py migrate
-# python manage.py collectstatic --no-input || echo "Static collection failed, continuing..."
-
-# Corrected if statement syntax
-# if [ "$CREATE_SUPERUSER" = "true" ]; then
-#     echo "Creating superuser"
-#     python manage.py createsuperuser --no-input
-# fi
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+gunicorn core.wsgi --bind 0.0.0.0:8000
