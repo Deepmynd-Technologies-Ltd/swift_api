6215728c18cf9523f05e85432e70735f463d0e95
Sobilo34 <bilalsolih60@gmail.com>
2025-02-09 07:29:28 +0100

    Update the code for deployment. Added the deployment files

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
+set -o errexit
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn core.wsgi --bind 0.0.0.0:8000
+
+
+if [[$CREATE_SUPERUSER == "true"]];
+then
+    echo "Creating superuser"
+    python manage.py createsuperuser --no-input
+fi
